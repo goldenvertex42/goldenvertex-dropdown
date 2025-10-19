1ced9dedfb3aea3bb2f38e2c06f03fcfc03c8217
goldenvertex42 <fabled93@gmail.com>
2025-10-19 15:55:09 -0500

    Add export in order to make npm package functional, thus updated version in package.json for republication

--- src/index.js
+++ src/index.js
@@ -2,22 +2,20 @@ import "./styles.css";
 import { DropdownMenu } from "./dropdown"
 import { setupEventListeners } from "./events";
 
-document.addEventListener('DOMContentLoaded', () => {
-    const dropdownElements = document.querySelectorAll('.dropdown');
-    const dropdownInstances = [];
+export const initialize = () => {
+    document.addEventListener('DOMContentLoaded', () => {
+        const dropdownElements = document.querySelectorAll('.dropdown');
+        const dropdownInstances = [];
 
-    dropdownElements.forEach(element => {
-        const dropdownInstance = new DropdownMenu(element);
-        dropdownInstances.push(dropdownInstance);
+        dropdownElements.forEach(element => {
+            const dropdownInstance = new DropdownMenu(element);
+            dropdownInstances.push(dropdownInstance);
 
-        dropdownInstance.button.addEventListener('click', () =>{
-            dropdownInstance.toggle();
+            dropdownInstance.button.addEventListener('click', () =>{
+                dropdownInstance.toggle();
+            });
         });
+
+        setupEventListeners(dropdownInstances);
     });
-
-    setupEventListeners(dropdownInstances);
-});
-
-exports.printMsg = function() {
-  console.log("This is a message from the demo package");
 }
